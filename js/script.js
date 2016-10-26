c8ad4853917be595b7cd34b57f6390c07e1ee9be
malilasage <malilaclearwater@gmail.com>
2016-10-25 22:01:59 -0600

    complete

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   var subtotal = $("#subtotal");
   var tax = $("#tax");
   var total = $("#total");
+  var tableBody = $(".table-body");
 
   $("div > #button").on("click", function (){
     var btn = $(this).parentsUntil("#card-main").children(".card-content");
@@ -11,7 +12,7 @@ $(document).ready(function () {
     var price = $("> p", btn).text();
     var priceNum = price.slice(1);
     //appending items to table
-    $(".table-body").append("<tr><td>" + food + "</td><td>" + price + "</td></tr>");
+    tableBody.append("<tr><td>" + food + "</td><td>" + price + "</td></tr>");
     //updating subtotal
     priceNum = parseFloat(priceNum);
     subNum += priceNum;
@@ -20,5 +21,14 @@ $(document).ready(function () {
     tax.html("$" + (subNum*taxRate).toFixed(2));
     //updating total
     total.html("$" + (subNum + (subNum*taxRate)).toFixed(2));
+  });
+
+  $("#submit").on("click", function() {
+    if (tableBody.children().length === 0 || $("input:empty").length !== 0) {
+      Materialize.toast('Order form not complete', 4000);
+    }
+    else{
+      Materialize.toast('Order submitted!', 4000);
+    }
   })
 })
