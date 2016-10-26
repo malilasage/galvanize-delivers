686c94e7cbd5ed019a12f2a7a6d6d606261ef642
malilasage <malilaclearwater@gmail.com>
2016-10-25 20:30:33 -0600

    items and prices apppend to table. subtotal variable updates each time

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
-
+    var subNum = 0;
   $("div > #button").on("click", function (){
     var btn = $(this).parentsUntil("#card-main").children(".card-content");
     var food = $("> h5", btn).text();
     var price = $("> p", btn).text();
+    var subtotal = $("#subtotal").text();
     $(".table-body").append("<tr><td>" + food + "</td><td>" + price + "</td></tr>");
-    $("#subtotal > p").text().replaceWith(price);
-  })
+      var priceNum = price.slice(1);
+      priceNum = parseFloat(priceNum);
+      subNum += priceNum;
+      // console.log(priceNum + 1);
 
+    // $("#subtotal > p").text().replaceWith(price);
+    // var sub = subtotal.text() + price;
+    // subtotal.text().replaceWith(sub);
+    // subtotal.replaceWith(subNum);
+
+    // console.log(subNum);
+
+  })
 })
